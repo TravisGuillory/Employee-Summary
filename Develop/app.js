274ff10e8aa333bd6804ba6a83a885f26fc89a1a
TravisGuillory <travisguillory70@gmail.com>
2020-03-31 23:45:37 -0500

    Added async inquirer prompts  per role

--- Develop/app.js
+++ Develop/app.js
@@ -11,6 +11,121 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+const employees = [];
+const confirm = [
+    {
+        type: "confirm",
+        message: "Add an employee?",
+        name: "confirm"
+    }
+]
+
+// Question for to determine job title
+
+
+
+// Questions to ask all employees
+const questions = [
+    {
+        type: "list",
+        message: "What is your job title?",
+        choices: ["Manager", "Engineer", "Intern"],
+        name: "role" 
+     },
+    
+    {
+        type: "input",
+        message: "What is  your name?",
+        name: "name"
+    },
+
+    {
+        type: "input",
+        message: "What is your id number?",
+        name: "id"
+    },
+
+    {
+        type: "input",
+        message: "what is your email address?",
+        name: "email"
+    }
+]
+
+// question for Managers
+
+const managerQuestion = [
+
+    {
+        type: "input", 
+        message: "What is your office number?",
+        name: "officeNumber"
+    }
+]
+
+const engineerQuestion = [
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "github"
+    }
+]
+
+const internQuestion = [
+    {
+        type: "input",
+        message: "What is the name of your school",
+        name: "school"
+    }
+]
+
+
+const init = async () => {
+    const addMember = await inquirer.prompt(confirm)
+    // If user confirms that an employee is to be added the proceed to questions
+    if (addMember.confirm){
+        const response = await inquirer.prompt(questions);
+        switch (response.role) {
+            case "Manager":
+                return manager(response);
+            case "Engineer":
+                return engineer(response);
+            case "Intern":
+                return intern(response);
+            default:
+                console.log("Incorrect emplopyee type");
+                break;
+        }
+
+    }else{
+        exit(employees);
+    }
+}
+
+const manager = async(data) =>{
+    const managerData = await inquirer.prompt(managerQuestion);
+    const elem = new Manager(data.name, data.id, data.email, managerData.officeNumber);
+    employees.push(elem);
+    console.log(employees);
+    init(); // Run init again to allow user to afdd another team member
+}
+
+const engineer = async(data) =>{
+    const engineerData = await inquirer.prompt(engineerQuestion);
+    const elem = new Engineer(data.name, data.id, data.email, engineerData.github);
+    employees.push(elem);
+    console.log(employees);
+    init();
+}
+
+const intern = async(data) => {
+    const internData = await inquirer.prompt(internQuestion);
+    const elem = new Intern(data.name, data.id, data.email, internData.school);
+    employees.push(elem);
+    console.log(employees);
+    init();
+}
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
@@ -32,4 +147,5 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work!```
 
-console.log("Hello World");
+
+init();
