95e63b53fe682364f0a11f8062b8e77645d0eb39
TravisGuillory <travisguillory70@gmail.com>
2020-04-01 01:09:31 -0500

    updated html style. Removed frivolous comments and console logs

--- Develop/app.js
+++ Develop/app.js
@@ -12,27 +12,24 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 const employees = [];
-const confirm = [
-    {
-        type: "confirm",
-        message: "Add an employee?",
-        name: "confirm"
-    }
-]
+const confirm = [{
+    type: "confirm",
+    message: "Add an employee?",
+    name: "confirm"
+}]
 
 // Question for to determine job title
 
 
 
 // Questions to ask all employees
-const questions = [
-    {
+const questions = [{
         type: "list",
         message: "What is your job title?",
         choices: ["Manager", "Engineer", "Intern"],
-        name: "role" 
-     },
-    
+        name: "role"
+    },
+
     {
         type: "input",
         message: "What is  your name?",
@@ -57,33 +54,29 @@ const questions = [
 const managerQuestion = [
 
     {
-        type: "input", 
+        type: "input",
         message: "What is your office number?",
         name: "officeNumber"
     }
 ]
 
-const engineerQuestion = [
-    {
-        type: "input",
-        message: "What is your GitHub username?",
-        name: "github"
-    }
-]
+const engineerQuestion = [{
+    type: "input",
+    message: "What is your GitHub username?",
+    name: "github"
+}]
 
-const internQuestion = [
-    {
-        type: "input",
-        message: "What is the name of your school",
-        name: "school"
-    }
-]
+const internQuestion = [{
+    type: "input",
+    message: "What is the name of your school",
+    name: "school"
+}]
 
 
 const init = async () => {
     const addMember = await inquirer.prompt(confirm)
     // If user confirms that an employee is to be added the proceed to questions
-    if (addMember.confirm){
+    if (addMember.confirm) {
         const response = await inquirer.prompt(questions);
         switch (response.role) {
             case "Manager":
@@ -97,34 +90,31 @@ const init = async () => {
                 break;
         }
 
-    }else{
+    } else {
         exit(employees);
     }
 }
 
 // functions to add the role specific question and push the new employee to the employees array
 
-const manager = async(data) =>{
+const manager = async (data) => {
     const managerData = await inquirer.prompt(managerQuestion);
     const elem = new Manager(data.name, data.id, data.email, managerData.officeNumber);
     employees.push(elem);
-    console.log(employees);
     init(); // Run init again to allow user to afdd another team member
 }
 
-const engineer = async(data) =>{
+const engineer = async (data) => {
     const engineerData = await inquirer.prompt(engineerQuestion);
     const elem = new Engineer(data.name, data.id, data.email, engineerData.github);
     employees.push(elem);
-    console.log(employees);
     init();
 }
 
-const intern = async(data) => {
+const intern = async (data) => {
     const internData = await inquirer.prompt(internQuestion);
     const elem = new Intern(data.name, data.id, data.email, internData.school);
     employees.push(elem);
-    console.log(employees);
     init();
 }
 
@@ -133,34 +123,17 @@ const intern = async(data) => {
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
 
-const exit = (team) =>{
-    fs.writeFile("./output/team.html", render(team), function(err){
+
+
+// generated html
+const exit = (team) => {
+    fs.writeFile("./output/team.html", render(team), function (err) {
         if (err) {
             throw err;
         }
         console.log("Created a team, team.html is cresated");
     })
 }
-
-
-
-
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
 
 
 init();
